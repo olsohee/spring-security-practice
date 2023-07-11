1f404438f073047a2652b57bfb1424345b8a4d48
olsohee <dlthgml0108@daum.net>
2023-07-11 23:23:44 +0900

    로그인, 회원가입 기능 개발

--- src/main/java/sh/security/config/WebSecurityConfig.java
+++ src/main/java/sh/security/config/WebSecurityConfig.java
@@ -5,7 +5,17 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
 
 @Configuration
 @RequiredArgsConstructor
@@ -26,7 +36,7 @@ public class WebSecurityConfig {
             // form을 이용한 로그인 필터 설정
             .formLogin(formLogin -> formLogin
                     .loginPage("/login")
-                    .defaultSuccessUrl("/home")
+                    .defaultSuccessUrl("/main")
                     .failureUrl("/login?fail")
                     .permitAll())
 
@@ -36,5 +46,21 @@ public class WebSecurityConfig {
                     .logoutSuccessUrl("/login"));
 
         return http.build();
+    }
+
+    @Bean
+    public UserDetailsManager userDetailsManager() {
+
+        // 테스터용 회원 생성
+        UserDetails user = User.withUsername("sohee")
+                .password(passwordEncoder().encode("1111"))
+                .build();
+
+        return new InMemoryUserDetailsManager(user);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
